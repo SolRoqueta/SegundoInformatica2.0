8eb4ab43bcc74031303f258c12265fe5ffc6c392
Nacho <CrucialP3@DESKTOP-VJNRR3M>
2024-10-03 20:05:29 -0300

    CREACION DISEÑOS menuProductos, menuReservas, y menuUsuarios

--- SegundoInformatica2/src/Presentacion/menuReservas.java
+++ SegundoInformatica2/src/Presentacion/menuReservas.java
@@ -1,28 +1,51 @@
 package Presentacion;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class menuReservas {
 
-	private JFrame frame;
+    public static void main(String[] args) {
+        // Create the frame
+        JFrame frame = new JFrame("Menu Reservas");
+        frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(309, 368);
+        frame.getContentPane().setLayout(null);
+        
+                // Welcome Label
+                JLabel labelTitulo = new JLabel("MENU RESERVAS", SwingConstants.CENTER);
+                labelTitulo.setBackground(Color.WHITE);
+                labelTitulo.setBounds(-1, -1, 295, 64);
+                frame.getContentPane().add(labelTitulo);
+                labelTitulo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+                labelTitulo.setBackground(Color.GRAY);
+                labelTitulo.setOpaque(true);
+                labelTitulo.setForeground(new Color(255, 255, 255));
+                
+                        // Boton para Agregar Productos
+                        JButton btnAgregarReservas = new JButton("Agregar");
+                        btnAgregarReservas.setBounds(80, 100, 125, 31);
+                        frame.getContentPane().add(btnAgregarReservas);
+                        
+                        // Boton para Modificar Productos
+                        JButton btnModificarReservas = new JButton("Modificar");
+                        btnModificarReservas.setBounds(80, 150, 125, 31);
+                        frame.getContentPane().add(btnModificarReservas);
+                                
+                        // Boton para Eliminar Productos
+                        JButton btnEliminarReservas = new JButton("Eliminar");
+                        btnEliminarReservas.setBounds(80, 200, 125, 31);
+                        frame.getContentPane().add(btnEliminarReservas);
+                        
+                        // Boton para Listar Productos
+                        JButton btnListarReservas = new JButton("Listar");
+                        btnListarReservas.setBounds(80, 250, 125, 31);
+                        frame.getContentPane().add(btnListarReservas);
 
-
-	/**
-	 * Create the application.
-	 */
-	public menuReservas() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
+        // Make the frame visible
+        frame.setVisible(true);
+    }
 }
--- SegundoInformatica2/src/Presentacion/menuUsuarios.java
+++ SegundoInformatica2/src/Presentacion/menuUsuarios.java
@@ -1,28 +1,51 @@
 package Presentacion;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class menuUsuarios {
 
-	private JFrame frame;
+    public static void main(String[] args) {
+        // Create the frame
+        JFrame frame = new JFrame("Menu Usuarios");
+        frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(309, 368);
+        frame.getContentPane().setLayout(null);
+        
+                // Welcome Label
+                JLabel labelTitulo = new JLabel("MENU USUARIOS", SwingConstants.CENTER);
+                labelTitulo.setBackground(Color.WHITE);
+                labelTitulo.setBounds(-1, -1, 295, 64);
+                frame.getContentPane().add(labelTitulo);
+                labelTitulo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+                labelTitulo.setBackground(Color.GRAY);
+                labelTitulo.setOpaque(true);
+                labelTitulo.setForeground(new Color(255, 255, 255));
+                
+                        // Boton para Agregar Productos
+                        JButton btnAgregarUsuarios = new JButton("Agregar");
+                        btnAgregarUsuarios.setBounds(80, 100, 125, 31);
+                        frame.getContentPane().add(btnAgregarUsuarios);
+                        
+                        // Boton para Modificar Productos
+                        JButton btnModificarUsuarios = new JButton("Modificar");
+                        btnModificarUsuarios.setBounds(80, 150, 125, 31);
+                        frame.getContentPane().add(btnModificarUsuarios);
+                                
+                        // Boton para Eliminar Productos
+                        JButton btnEliminarUsuarios = new JButton("Eliminar");
+                        btnEliminarUsuarios.setBounds(80, 200, 125, 31);
+                        frame.getContentPane().add(btnEliminarUsuarios);
+                        
+                        // Boton para Listar Productos
+                        JButton btnListarUsuarios = new JButton("Listar");
+                        btnListarUsuarios.setBounds(80, 250, 125, 31);
+                        frame.getContentPane().add(btnListarUsuarios);
 
-
-	/**
-	 * Create the application.
-	 */
-	public menuUsuarios() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
+        // Make the frame visible
+        frame.setVisible(true);
+    }
 }
