de805388e26e91bb3cebaa3d080ed2d76fbf694e
CrucialP3 <CrucialP3@DESKTOP-VJNRR3M>
2024-10-09 13:42:56 -0300

    FUNCIONAMIENTO BOTONES MENU PRICIPAL

--- SegundoInformatica2/src/Presentacion/menuPrincipal.java
+++ SegundoInformatica2/src/Presentacion/menuPrincipal.java
@@ -5,46 +5,77 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class menuPrincipal {
-
-    public static void main(String[] args) {
-        // Create the frame
-        JFrame frame = new JFrame("Menu Principal");
-        frame.getContentPane().setBackground(Color.LIGHT_GRAY);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(309, 368);
-        frame.getContentPane().setLayout(null);
+public class menuPrincipal extends JFrame{
+	
+	private int limiteVentana = 0;
+	
+	private menuProductos ventanaProductos;
+    private menuUsuarios  ventanaUsuarios;
+    private menuReservas ventanaReservas;
+	
+	public menuPrincipal() {
+  
+		setTitle("Menu Principal");
+		setSize(309, 368);
+		getContentPane().setBackground(Color.LIGHT_GRAY);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
         
                 // Welcome Label
                 JLabel welcomeLabel = new JLabel("MENU PRICIPAL", SwingConstants.CENTER);
                 welcomeLabel.setBackground(Color.WHITE);
                 welcomeLabel.setBounds(-1, -1, 295, 64);
-                frame.getContentPane().add(welcomeLabel);
+                getContentPane().add(welcomeLabel);
                 welcomeLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
                 welcomeLabel.setBackground(Color.GRAY);
                 welcomeLabel.setOpaque(true);
                 welcomeLabel.setForeground(new Color(255, 255, 255));
                 
+               
+                // Button for Menu Reservas
+                JButton reservasButton = new JButton("Menu Reservas");
+                reservasButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                    	ventanaReservas = new menuReservas(); 
+                    }
+                });
+                    
+                reservasButton.setBounds(78, 100, 133, 31);
+                getContentPane().add(reservasButton);
+                        
+                // Button for Menu Usuarios
+                JButton usuariosButton = new JButton("Menu Usuarios");
+                usuariosButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+            		
+            		menuUsuarios MenuUsuarios = new menuUsuarios();
+            		
+            	}
+            });
+            usuariosButton.setBounds(78, 170, 133, 31);
+            getContentPane().add(usuariosButton);      
+                
                         // Button for Menu Productos
                         JButton productosButton = new JButton("Menu Productos");
                         productosButton.addActionListener(new ActionListener() {
                         	public void actionPerformed(ActionEvent e) {
+                        		
+                        		menuProductos MenuProductos = new menuProductos();
+                        		
                         	}
                         });
-                        productosButton.setBounds(85, 240, 122, 31);
-                        frame.getContentPane().add(productosButton);
+                        productosButton.setBounds(78, 240, 133, 31);
+                        getContentPane().add(productosButton);
                         
-                                // Button for Menu Reservas
-                                JButton reservasButton = new JButton("Menu Reservas");
-                                reservasButton.setBounds(85, 173, 122, 31);
-                                frame.getContentPane().add(reservasButton);
-                                
-                                        // Button for Menu Usuarios
-                                        JButton usuariosButton = new JButton("Menu Usuarios");
-                                        usuariosButton.setBounds(85, 105, 122, 31);
-                                        frame.getContentPane().add(usuariosButton);
-
-        // Make the frame visible
-        frame.setVisible(true);
+	}
+	
+                        public static void main(String[] args) {
+                        	// Show the main window
+                        	menuPrincipal MenuPrincipal = new menuPrincipal();
+                        	MenuPrincipal.setVisible(true);
+                        
     }
+	
 }
+
