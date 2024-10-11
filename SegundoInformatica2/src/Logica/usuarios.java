63d6b22ce1c6c194761a09034d53cf1efbe1e912
nacho <nacho@LAPTOP-MAK7MFGD>
2024-10-11 11:07:39 -0300

    MODIFICACION CLASE USUARIOS y ELIMINACION USUARIO PROFE Y PADRE
    Ahora las dos clases de usuarios (padre y profe) estan unidas en la
    clase usuarios, cambie los atributos y ahora los usuarios tienen un id,
    nombre, contraseña, mail y tipo de usuario (1 padre, 2 profe)

--- SegundoInformatica2/src/Logica/usuarios.java
+++ SegundoInformatica2/src/Logica/usuarios.java
@@ -8,65 +8,95 @@ import java.util.Scanner;
 
 public class usuarios {
 	
+	private int idUsuarios; //Declaro el int de id
+	private String nombreUsuario; //Declaro el String de nombre_completo
+	private String contrasenha; //Declaro el String de contraseña
+	private int tipoUsuario; 
+	private String mail;
 	
-	private int id_usuarios; //Declaro el int de id
-	private String nombre_completo; //Declaro el String de nombre_completo
-	private String CI; //Declaro el String de la cedula de identidad
+
+	 // CONSTRUCTOR DE USUARIOS 
 	
-	/*
-	 *		CONSTRUCTOR DE USUARIOS 
-	 */
-	
-	public usuarios(String nom_comp, String ci) {
+	public usuarios(String nombUsu, String cont) {
 		
-		nombre_completo = nom_comp;
-		CI = ci;
-		
+		nombreUsuario = nombUsu;
+		contrasenha = cont;
+
 	}
 	
 	public usuarios() {
 		
-		
-		
 	}
 	
-	/*
-	 * 		GETS Y SETS DE LOS ATRIBUTOS
-	 */
-
-	public String getNombre() {
-		
-		return nombre_completo;
-		
-	}
-	
-	public void setNombre(String NOMBRE_COMPLETO) {
-		
-		nombre_completo = NOMBRE_COMPLETO;
-		
-	}
-	
-	public String getCI() {
-		
-		return CI;
-		
-	}
-	
-	public void setCI(String ci) {
-		
-		CI = ci;
-		
-	}
+	 // GETS Y SETS DE LOS ATRIBUTOS
 	
 	public int getId() {
-		
-		return id_usuarios;
-		
+		return idUsuarios;
 	}
 	
 	public void setId(int ID) {
-		
-		id_usuarios = ID;
+		idUsuarios = ID;
+	}
+
+	public String getNombre() {
+		return nombreUsuario;
+	}
+	
+	public void setNombre(String NOMBRE_USUARIO) {
+		nombreUsuario = NOMBRE_USUARIO;
+	}
+	
+	public String getCont() {
+		return contrasenha;
+	}
+	
+	public void setCont(String cont) {
+		contrasenha = cont;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String email) {
+		mail = email;
+	}
+	
+	public int getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(int tipoUsu) {
+		tipoUsuario = tipoUsu;
+	}
+	
+	public void AgregarUsuario() {
+	       
+	     conexion cc = new conexion();
+	     Connection con = cc.conect();
+			
+	     //Sentencia SQL que inserta los datos en la tabla "usuarios"
+	     String query = "INSERT INTO usuarios (nombreCompleto, contrasenha, tipoUsuario, mail) VALUES (?, ?, ?, ?);";
+	     
+	     try (Connection connection = con;
+				 PreparedStatement statement = connection.prepareStatement(query)) {
+				
+					statement.setString(1, this.getNombre());
+					statement.setString(2, this.getCont());
+					statement.setInt(3, this.getTipoUsuario());
+					statement.setString(4, this.getMail());
+					
+					int rowsInserted = statement.executeUpdate();
+					if (rowsInserted > 0) {
+						System.out.println("Ingresado con exito!");
+					}
+				
+			} catch (SQLException ex) {
+				
+//				ex.printStackTrace();
+				System.out.println("ERROR, al agregar usuario");
+			
+			}
 		
 	}
 	
@@ -76,12 +106,12 @@ public class usuarios {
 	    Connection con = cc.conect();
 			
 	     //Sentencia SQL que elimina los datos en la tabla "productos" de la fila del usuario cullo nombre sea del objeto
-	     String query = "DELETE FROM usuarios WHERE nombre_completo = ? LIMIT 1;";
+	     String query = "DELETE FROM usuarios WHERE idUsuarios = ? LIMIT 1;";
 	     
 	     try (Connection connection = con;
 				 PreparedStatement statement = connection.prepareStatement(query)) {
 				
-					statement.setString(1, nombre_completo);
+					statement.setInt(1, idUsuarios);
 					
 					int rowsUpdated = statement.executeUpdate();
 					if (rowsUpdated > 0) {
@@ -101,23 +131,40 @@ public class usuarios {
 		
 	}
 	
-	/*
-	 * 						CREO LA FUNCION BuscarProducto
-	 * 
-	 *  Como se puede ver, tiene dos inputs, uno que es tempNombre_completo y 
-	 *  otro que es opcion, en TEMPNOMBRE_COMPLETO, se debe de poner el nombre del
-	 *  usuario que el usuario desea buscar. OPCION se utiliza para indicarle
-	 *  al programa si el usuario quiere hacer un select para mostrar todos
-	 *  los usuario o si quiere buscar uno en específico. Se debe de insertar
-	 *  1 en el caso de querer buscar un usuario especifico o 2 en el caso de 
-	 *  queres buscar todos los usuarios de la base de datos.
-	 *  
-	 *  Esta funcion retorna un valor de tipo usuarios, para que cuando se
-	 *  busque uno especifico se pueda setear el ex-objeto de usuarios con el
-	 *  nuevo objeto de usuarios (tempUsuario).
-	 *  
-	 */
-	
-	
+	public void ModificarUsuario() {
+		
+		conexion cc = new conexion();
+	    Connection con = cc.conect();
+			
+	     //Sentencia SQL que inserta los datos en la tabla "usuario"
+	     String query = "UPDATE usuarios SET nombreCompleto = ?, contrasenha = ?, mail = ?, tipoUsuario = ? WHERE idUsuarios = ?";
+	     
+	     try (Connection connection = con;
+				 PreparedStatement statement = connection.prepareStatement(query)) {
+				
+					statement.setString(1, this.getNombre());
+					statement.setString(2, this.getCont());
+					statement.setString(3, this.getMail());
+					statement.setInt(4, this.getTipoUsuario());
+					statement.setInt(5, this.getId());
+					
+					int rowsUpdated = statement.executeUpdate();
+					if (rowsUpdated > 0) {
+						System.out.println("Actualizado con exito!");
+					} else if (rowsUpdated == 0) {
+						
+						System.out.println("No actualizado");
+						
+					}
+				
+			} catch (SQLException ex) {
+				
+				ex.printStackTrace();
+				System.out.println("No se a podido modificar el usuario");
+			
+			}
+		
+	}
+
 	
 }
