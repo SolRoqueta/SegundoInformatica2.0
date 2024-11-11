4882b43ded5d528320082538159911ffecf5bb30
Zeta <mateozetazeballos@gmail.com>
2024-11-11 12:21:13 -0300

    PAGINA INICIO
    Ya se cargan los datos de la base de datos en la pagina inicio

--- InicioPrueba/script.js
+++ InicioPrueba/script.js
@@ -3,6 +3,67 @@ window.addEventListener("DOMContentLoaded", () => {
     showLoader(); 
 }) 
 
+document.addEventListener('DOMContentLoaded', startEvents, true);
+
+function startEvents() {
+
+    getProducts();
+
+}
+
+var conection_fixedMenus
+function getFixedMenus() {
+
+    conection_fixedMenus = new XMLHttpRequest();
+    conection_fixedMenus.onreadystatechange = proccessFixedMenus;
+    conection_fixedMenus.open('GET', 'inicio.php?func=fixedMenus', true);
+    conection_fixedMenus.send();
+
+}
+
+function proccessFixedMenus() {
+
+    var fixedMenus = document.getElementById('fixedMenu');
+
+    if (conection_fixedMenus.readyState == 4 && conection_fixedMenus.status == 200) {
+
+        fixedMenus.innerHTML = conection_fixedMenus.responseText;
+
+    } else {
+
+        fixedMenus.innerHTML = "cargando...";
+
+    }
+
+}
+
+var conection_products
+function getProducts() {
+
+    conection_products = new XMLHttpRequest();
+    conection_products.onreadystatechange = proccessProducts;
+    conection_products.open('GET', 'inicio.php?func=products', true);
+    conection_products.send();
+
+}
+
+function proccessProducts() {
+
+    var products = document.getElementById('products');
+
+    if (conection_products.readyState == 4 && conection_products.status == 200) {
+
+        products.innerHTML = conection_products.responseText;
+        getFixedMenus();
+
+    } else {
+
+        products.innerHTML = "cargando...";
+
+    }
+
+}
+
 // Espera que carguen los estilos
 window.addEventListener("load", () => { 
     setTimeout(() => { 
