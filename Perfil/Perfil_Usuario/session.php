710f9435d51080f19a5485f5ec1b0dcab22de943
ZetaGameDev <mateozetagamedev@gmail.com>
2024-11-25 00:30:00 -0300

    asasd
    asdasdasd

---
+++ Perfil/Perfil_Usuario/session.php
@@ -0,0 +1,67 @@
+<?php
+
+    header('Content-Type: application/json');
+
+    session_start();
+
+    $data = [];
+
+    $option = $_REQUEST['option'];
+
+    switch ($option) {
+
+        case 1:
+
+            if (isset($_SESSION['name']) && isset($_SESSION['mail'])) {
+
+                $name = $_SESSION['name'];
+                $mail = $_SESSION['mail'];
+        
+                $data[] = [
+        
+                    'nombre' => $name,
+                    'mail' => $mail
+            
+                ];
+            
+                echo json_encode($data);
+        
+            } else {
+        
+                echo null;
+        
+            }
+
+            break;
+
+        case 2:
+
+            $name = $_REQUEST['name'];
+
+            $_SESSION['name'] = $name;
+
+            $name = $_SESSION['name'];
+            $mail = $_SESSION['mail'];
+        
+            $data[] = [
+        
+                'nombre' => $name,
+                'mail' => $mail
+            
+            ];
+
+            echo json_encode($data);
+
+            break;
+
+        case 3:
+
+            $mail = $_REQUEST['mail'];
+
+            $_SESSION['mail'] = $mail;
+
+            break;
+
+    }
+
+?>
